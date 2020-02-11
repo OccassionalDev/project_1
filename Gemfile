e708119460792fa84ed52f9ea34d254220eea3b8
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-11 20:46:54 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in bass_tournaments.gemspec
+gemspec
---
+++ lib/bass_tournaments.rb
@@ -0,0 +1,6 @@
+require "bass_tournaments/version"
+
+module BassTournaments
+  class Error < StandardError; end
+  # Your code goes here...
+end
