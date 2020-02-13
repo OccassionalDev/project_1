ca6bba08951b2550e8302d384c6538223acdfe2d
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-13 04:03:05 +0000

    added method and helper methods to scrape tournament data in Scraper and create objects through arrays in Tournaments in bass_tournaments.rb

--- lib/bass_tournaments.rb
+++ lib/bass_tournaments.rb
@@ -1,6 +1,10 @@
+require "nokogiri"
+require "open-uri"
+
 class CLI
 
   def start 
+    Scraper.new.scrape_tournaments
     puts "Hello! Welcome to the Bassmaster Tournament app."
     main_menu
   end 
@@ -18,7 +22,8 @@ class CLI
     case input
     
       when "1"
-        puts "working"
+        puts Tournament.all 
+        main_menu
       
       when "2"
         exiting_program_message
@@ -33,9 +38,63 @@ class CLI
   end 
 end 
 
+class Tournament 
+  attr_accessor :name, :date, :series, :location 
+  
+  @@all = [] 
+  
+  def create_from_array(array)
+    @series = array[0]
+    @name = array[1]
+    @location = array[2]
+    @date = array[3]
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+end 
+
+
+
 class Scraper
-
-
+  
+  def scrape_tournaments 
+    tournament_table_data = create_array_of_table_text
+    
+    tournament_table_data.each do |table_row_data|
+      Tournament.new.create_from_array(table_row_data)
+    end 
+  end 
+  
+  def create_array_of_table_text
+    doc = Nokogiri::HTML(open("https://www.bassmaster.com/bassmaster-tournament-schedule"))
+    
+    # Grab the table from the website
+    tournament_table = doc.css("table.views-table.cols-5.table.table-striped.table-bordered")
+    
+    # Store all the rows in to a variable 
+    table_rows = tournament_table.css("tr")
+    
+    # Store the text from the rows in a nested array 
+    # [[first row data], [second row data]]
+    
+    all_text_from_table = table_rows.collect do |row|
+      
+      # Go through each of the rows and collect the data as text and store it as a array
+      row_text = row.css("td").collect { |data| data.text }
+      
+      # Place the new array in to the array 
+      [row_text]
+    end 
+    
+    # Return the nested array 
+    all_text_from_table
+  end 
 end 
 
 CLI.new.start
